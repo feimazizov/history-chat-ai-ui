6a677224b5c1e73f2f4803beed3321b112983abd
your.user.name <nedjatymehmed@gmail.com>
2025-07-21 12:07:37 +0300

    initial setup of the ui app

---
+++ hisotry-chat-ai-ui/src/StopMessage.tsx
@@ -0,0 +1,11 @@
+export default function StopMessage({ count }: { count: number }) {
+    return (<div>{
+        count > 5 &&
+        <>Stop!</>
+    }
+        {
+            count < 5 &&
+            <>Go on!</>
+        }
+    </div>)
+}
