522618ebbe2f52330a98556fe0daf144251043dd
your.user.name <nedjatymehmed@gmail.com>
2025-07-21 12:12:13 +0300

    format message component

--- hisotry-chat-ai-ui/src/StopMessage.tsx
+++ hisotry-chat-ai-ui/src/StopMessage.tsx
@@ -1,8 +1,9 @@
 export default function StopMessage({ count }: { count: number }) {
-    return (<div>{
-        count > 5 &&
-        <>Stop!</>
-    }
+    return (<div>
+        {
+            count > 5 &&
+            <>Stop!</>
+        }
         {
             count < 5 &&
             <>Go on!</>
